6eb9645fb81ad69eb81a6dbe1f0308c85d814773
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-09-09 11:55:35 +0530

    import java.util.Scanner; class Division {     public static void main(String args[])     {         int n,c=0,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(arr[i]%2!=0)             {                 c++;             }         }         if(c<=2)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }         sc.close();     } }

---
+++ Can_Robber_Escape?.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Division
+{
+    public static void main(String args[])
+    {
+        int n,c=0,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                c++;
+            }
+        }
+        if(c<=2)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
