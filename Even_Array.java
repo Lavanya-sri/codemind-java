1c9a55398f42513d2c53d631c902f8018326597f
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-09-13 20:08:09 +0530

    import java.util.Scanner; class Prog {     public static void main(String args[])     {         int n,c=0,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(arr[i]%2==0)             {                 c++;             }         }         if(c==n)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ Even_Array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Prog
+{
+    public static void main(String args[])
+    {
+        int n,c=0,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                c++;
+            }
+        }
+        if(c==n)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
