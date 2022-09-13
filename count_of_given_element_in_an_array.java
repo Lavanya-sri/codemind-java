c1770f98aeb18a1c8c1470425028798939b7dc4b
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-09-13 20:27:50 +0530

    import java.util.Scanner; class Prog {     public static void main(String args[])     {         int n,c=0,i,k;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(arr[i]==k)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Prog
+{
+    public static void main(String args[])
+    {
+        int n,c=0,i,k;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]==k)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
