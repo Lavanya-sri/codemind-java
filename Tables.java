05e1ea80dfebb6a808303154058c4b7f04af0470
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-09-16 09:55:21 +0530

    import java.util.Scanner; class Prog {     public static void main(String args[])     {         int a,b,i;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         for(i=1;i<=b;i++)         {             if(i%2!=0)             {                 System.out.println(a+" "+"x"+" "+i+" "+"="+" "+a*i);             }         }     } }

---
+++ Tables.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Prog
+{
+    public static void main(String args[])
+    {
+        int a,b,i;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=1;i<=b;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(a+" "+"x"+" "+i+" "+"="+" "+a*i);
+            }
+        }
+    }
+}
