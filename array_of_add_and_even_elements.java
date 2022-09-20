d9c2239e962860e6210386892628e982874071ca
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-09-20 20:32:50 +0530

    import java.util.Scanner; class prog {     public static void main(String args[])     {         int n,i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(arr[i]%2!=0)             {                 System.out.print(arr[i]+" ");             }         }         for(i=0;i<n;i++)         {             if(arr[i]%2==0)             {                 System.out.print(arr[i]+" ");             }         }
        }
    }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class prog
+{
+    public static void main(String args[])
+    {
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        
+    }
+}
