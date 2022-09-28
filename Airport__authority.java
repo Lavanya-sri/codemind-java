6aed477a174fc89231728b9a8b47c299a51f6ec2
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-09-28 22:15:06 +0530

    import java.util.Scanner; class palindrom {     public static void main(String args[])     {         int n,count=0,k,countt=0,counttt=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int [] arr=new int[n];         for(int i=0;i<n;i++)         {            arr[i]=sc.nextInt();         }         k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]<=k)             {                 count+=1;             }             else             {                 countt+=2;             }         }         counttt=count+countt;         System.out.println(counttt);         sc.close();     } }

---
+++ Airport__authority.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class palindrom
+{
+    public static void main(String args[])
+    {
+        int n,count=0,k,countt=0,counttt=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+           arr[i]=sc.nextInt(); 
+        }
+        k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=k)
+            {
+                count+=1;
+            }
+            else
+            {
+                countt+=2;
+            }
+        }
+        counttt=count+countt;
+        System.out.println(counttt);
+        sc.close();
+    }
+}
